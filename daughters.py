d3bd7d77c6584a265a7ca76c36e0345d94a0dc41
Feng, Wen Lung (David) <dfeng99@users.noreply.github.com>
2018-11-06 19:05:54 +0800

    homework-計算生女兒機率

---
+++ daughters.py
@@ -0,0 +1,39 @@
+import numpy as np
+import sys
+
+
+def get_opportunities(families, show_detail=False):
+    daughters = 0
+    for family in range(0, families):
+        children = 0
+        is_son = True
+        if show_detail:
+            sys.stdout.write(str(family)+":")
+        while is_son and children < 3:
+            rn = np.random.randint(0,99)
+            if show_detail:
+                sys.stdout.write("("+str(rn)+")")
+
+            children += 1
+
+            if rn <= 48:
+                daughters += 1
+                is_son = False
+
+            if not is_son:
+                if show_detail:
+                    sys.stdout.write('女+')
+            elif children == 3:
+                if show_detail:
+                    sys.stdout.write("男")
+            else:
+                if show_detail:
+                    sys.stdout.write("男")
+        if show_detail:
+            print(" ")
+
+    return daughters/families
+
+
+print("Opportunity if 10 families try: ", get_opportunities(10, True))
+print("Opportunity if 10,000 families try: ", get_opportunities(10000))
